b7ccf3cf182aa34a48376cdbacdfde178322bec5
Martin Gausby <martin@gausby.dk>
2016-05-24 17:17:08 +0200

    Deleted some old todo comments

--- lib/matsou.ex
+++ lib/matsou.ex
@@ -2,10 +2,4 @@ defmodule Matsou do
   @moduledoc """
   An Elixir wrapper around Riak CRDT types.
   """
-
-  # should work as plug, a pipeline where operations can be done
-  # should fail if a value that isn't in the schema is set
-  # should fail if a dirty value fail to validate
-
-  # counters, should be able to set, increment, or decrement value probably with a fn old -> new end
 end
